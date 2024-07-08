cadd8a305dd05aff2b55e5f2c12605b3fadb35b2
JAGeorge54 <jesse.george54@gmail.com>
2024-07-08 17:24:36 -0400

    added link to navbar brand

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -9,7 +9,7 @@ function Navigation() {
     return(
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand href="#home">Jesse George</Navbar.Brand>
+          <Navbar.Brand as={ Link } to='/'>Jesse George</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={ Link } to='/'>Home</Nav.Link>
             <Nav.Link as={ Link } to='/projects'>Projects</Nav.Link>
