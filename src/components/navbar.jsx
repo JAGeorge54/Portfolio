bc1b46e8a18f143c69e07bcf33247dac5036299d
JAGeorge54 <jesse.george54@gmail.com>
2024-07-09 06:23:35 -0400

    added a and img tag for navbar brand monogram

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,13 +3,20 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom'
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import jgmonogram from '../assets/jgmonogram.jpg'
 
 function Navigation() {
 
     return(
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
-          <Navbar.Brand as={ Link } to='/'>Jesse George</Navbar.Brand>
+          <Navbar.Brand as={ Link } to='/'>
+          <a>
+            <img className="d-inline-block align-top"
+            src={jgmonogram}
+            width="75" height="75" />
+          </a>
+          </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={ Link } to='/'>Home</Nav.Link>
             <Nav.Link as={ Link } to='/projects'>Projects</Nav.Link>
