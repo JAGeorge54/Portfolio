ab69969a7bfc97c75633c8a0b1e3a7cc481e16b9
JAGeorge54 <jesse.george54@gmail.com>
2024-07-10 08:46:46 -0400

    added experience component

---
+++ src/components/experience.jsx
@@ -0,0 +1,16 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
+
+function Experience() {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                text
+            </Card>
+        </>
+    )
+}
+
+export default Experience;
