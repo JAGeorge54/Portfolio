9bdb4949d66cd7ddd350c7a13ef298d303807a6a
JAGeorge54 <jesse.george54@gmail.com>
2024-07-10 08:47:06 -0400

    added hobbies component

---
+++ src/components/hobbies.jsx
@@ -0,0 +1,17 @@
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card'
+
+
+function Hobbies() {
+    return (
+        <>
+            <Card style={{ width: '18rem' }}>
+                text
+            </Card>
+        </>
+    )
+}
+
+export default Hobbies;
