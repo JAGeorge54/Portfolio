0723d68bbfc146a7ec6cbe36554c014c7175f67d
JAGeorge54 <jesse.george54@gmail.com>
2024-07-10 08:48:04 -0400

    added second row for new components

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,5 +1,7 @@
 import './home.css'
 import BioCard from '../components/bioCard';
+import Hobbies from '../components/hobbies';
+import Experience from '../components/experience';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -9,13 +11,21 @@ function Home() {
         <>
         <Container>
             <Row className="justify-content-center">
-                <Col sm="auto">
+                <Col md={3}sm="auto">
                 <BioCard />
                 </Col>
-                <Col md={8} >
+                <Col md={4} className='video1'>
                 <div className="ratio ratio-16x9">
                     <iframe src="https://www.youtube.com/embed/ty_mlNuzll8?si=UF64PMYEl8TCKHc_" title="YouTube video" allowfullscreen></iframe>
                 </div>
+                </Col>
+            </Row>
+            <Row>
+                <Col>
+                    <Hobbies />
+                </Col>
+                <Col>
+                    <Experience />
                 </Col>
             </Row>
         </Container>
