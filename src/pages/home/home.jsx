ec4d2d03b9aab59dc5d7ba0cfad8fdfb492acd55
JAGeorge54 <jesse.george54@gmail.com>
2024-07-17 11:07:03 -0400

    added style and biocard component

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,12 +1,25 @@
+import BioCard from '../../components/bioCard'
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 
 function Home() {
     return (
-        <>
-            Home Page
-        </>
+        <div className='App'>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'center',
+                padding: '5px'
+
+            }}
+            >
+                <div>
+                    <BioCard />
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
