9f00bdbc6cfe16e17a7a123779088595160a57fb
JAGeorge54 <jesse.george54@gmail.com>
2024-07-19 10:57:28 -0400

    adding cards for projects

--- src/pages/projects/Projects.jsx
+++ src/pages/projects/Projects.jsx
@@ -1,8 +1,51 @@
+import Card from 'react-bootstrap/Card';
+
 function Projects() {
     return (
-        <>
-        Projects page
-        </>
+        <div style={{
+            display: 'flex',
+            justifyContent: 'space-evenly',
+            width: 1500,
+            height: 1500,
+            alignItems:'center'
+        }}>
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+            <div>
+                <Card style={{ width: '18rem' }}>
+                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <Card.Body>
+                        <Card.Title>Card Title</Card.Title>
+                        <Card.Text>
+                        Some quick example text to build on the card title and make up the
+                        bulk of the card's content.
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
     )
 }
 
