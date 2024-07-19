598db93021dbb22ae93ad41b8ca2958728724b7d
JAGeorge54 <jesse.george54@gmail.com>
2024-07-19 09:33:51 -0400

    fixed a tag error

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -11,11 +11,9 @@ function Navigation() {
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand as={ Link } to='/'>
-          <a>
             <img className="d-inline-block align-top"
             src={jgmonogram}
             width="75" height="75" />
-          </a>
           </Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link as={ Link } to='/'>Home</Nav.Link>
