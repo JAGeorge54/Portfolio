315078b74db87a44c834238a4033723f404a834d
JAGeorge54 <jesse.george54@gmail.com>
2024-07-19 10:20:33 -0400

    added cards for experience and hobbies

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,6 @@
 import BioCard from '../../components/bioCard'
 import YoutubeEmbed from '../../components/youTubeEmbed';
+import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -10,12 +11,63 @@ function Home() {
             <div style={{
                 display: 'flex',
                 justifyContent: 'center',
+                flexDirection: 'column'
             }}
             >
-                <div style={{display: 'flex'
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'center'
                 }}>
                     <BioCard />
                     <YoutubeEmbed embedId='ty_mlNuzll8?si=IdfUortm0KZ90j_v'/>
+                </div>
+                <div style={{
+                    display: 'flex',
+                    justifyContent: 'space-evenly'
+                }}>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                        
+                    }}>
+                        <Card
+                            bg='secondary'
+                            key='secondary'
+                            text='white'
+                            style={{ width: '18rem'}}
+                            className="mb-2"
+                            >
+                            <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    <div style={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <Card
+                            bg='secondary'
+                            key='secondary'
+                            text='white'
+                            style={{ width: '18rem' }}
+                            className="mb-2"
+                            >
+                            <Card.Header>Header</Card.Header>
+                            <Card.Body>
+                                <Card.Title> Card Title </Card.Title>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the
+                                bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </div>
                 </div>
             </div>
 
