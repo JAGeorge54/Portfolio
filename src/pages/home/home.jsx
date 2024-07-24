950e216f23a0a1dbd3ba004b424632cb8d6ec889
JAGeorge54 <jesse.george54@gmail.com>
2024-07-24 16:08:20 -0400

    removed unnecessary code

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -11,7 +11,7 @@ function Home() {
             <div style={{
                 display: 'flex',
                 justifyContent: 'center',
-                flexDirection: 'column'
+                flexDirection: 'column',
             }}
             >
                 <div style={{
@@ -25,49 +25,38 @@ function Home() {
                     display: 'flex',
                     justifyContent: 'space-evenly'
                 }}>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center'
-                        
-                    }}>
-                        <Card
-                            bg='secondary'
-                            key='secondary'
-                            text='white'
-                            style={{ width: '18rem'}}
-                            className="mb-2"
-                            >
-                            <Card.Header>Header</Card.Header>
-                            <Card.Body>
-                                <Card.Title> Card Title </Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </div>
-                    <div style={{
-                        display: 'flex',
-                        justifyContent: 'center'
-                    }}>
-                        <Card
-                            bg='secondary'
-                            key='secondary'
-                            text='white'
-                            style={{ width: '18rem' }}
-                            className="mb-2"
-                            >
-                            <Card.Header>Header</Card.Header>
-                            <Card.Body>
-                                <Card.Title> Card Title </Card.Title>
-                                <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
-                    </div>
+                    <Card
+                        bg='secondary'
+                        key='secondary'
+                        text='white'
+                        style={{ width: '18rem'}}
+                        className="mb-2"
+                        >
+                        <Card.Header>Header</Card.Header>
+                        <Card.Body>
+                            <Card.Title> Card Title </Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
+                    <Card
+                        bg='secondary'
+                        key='secondary'
+                        text='white'
+                        style={{ width: '18rem' }}
+                        className="mb-2"
+                        >
+                        <Card.Header>Header</Card.Header>
+                        <Card.Body>
+                            <Card.Title> Card Title </Card.Title>
+                            <Card.Text>
+                            Some quick example text to build on the card title and make up the
+                            bulk of the card's content.
+                            </Card.Text>
+                        </Card.Body>
+                    </Card>
                 </div>
             </div>
 
