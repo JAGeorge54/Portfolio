e40964a911bcd0c42981e20ff46f2a413934fe47
JAGeorge54 <jesse.george54@gmail.com>
2024-07-25 16:17:31 -0400

    moved inline style to style sheet

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,6 +1,7 @@
 import BioCard from '../../components/bioCard'
 import YoutubeEmbed from '../../components/youTubeEmbed';
 import Card from 'react-bootstrap/Card';
+import ListGroup from 'react-bootstrap/ListGroup';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -8,12 +9,7 @@ import Col from 'react-bootstrap/Col';
 function Home() {
     return (
         <div className='content'>
-            <div style={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexDirection: 'column',
-            }}
-            >
+            <div>
                 <div style={{
                     display: 'flex',
                     justifyContent: 'center'
@@ -33,13 +29,13 @@ function Home() {
                         style={{ width: '18rem'}}
                         className="mb-2"
                         >
-                        <Card.Header>Header</Card.Header>
+                        <Card.Header>Hobbies</Card.Header>
                         <Card.Body>
-                            <Card.Title> Card Title </Card.Title>
-                            <Card.Text>
-                            Some quick example text to build on the card title and make up the
-                            bulk of the card's content.
-                            </Card.Text>
+                            <ListGroup variant='flush'>
+                                <ListGroup.Item variant='dark'>Cras justo odio</ListGroup.Item>
+                                <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                                <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                            </ListGroup>
                         </Card.Body>
                     </Card>
                     <Card
