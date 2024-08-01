8b2079dd7747f8c90ec14a161fb95875ada3ed0f
JAGeorge54 <jesse.george54@gmail.com>
2024-08-01 12:36:25 -0400

    added content for 404 not found page

--- src/pages/notFound.jsx
+++ src/pages/notFound.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 function notFound() {
     return (
-        <div>
-            <h1>404 Page Not Found</h1>
+        <div className='notFoundContent'>
+            <h1>Error: 404</h1>
+            <h2>Page Not Found</h2>
+            <h1>¯\_(ツ)_/¯</h1>
+            <br />
+            <br />
+            <Link className='goHome' to='/'>Back Home</Link>
         </div>
     );
 }
